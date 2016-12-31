f90b178b298e9f4df1a24ce7717c7ca6b4c1a13a
Enrico Tröger <enrico.troeger@uvena.de>
2016-12-31 13:36:26 +0100

    Keep the HTML theme at "default" for ReadTheDocs

--- docs/conf.py
+++ docs/conf.py
@@ -84,7 +84,10 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'classic'
+if os.environ.get('READTHEDOCS') == 'True':
+    html_theme = 'default'
+else:
+    html_theme = 'classic'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
