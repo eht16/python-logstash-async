2904bb6751b37c8beb3a354b7e05244c17152c66
Jürgen Löhel <juergen.loehel@inlyse.com>
2020-07-08 21:18:58 -0500

    Updates example
    Signed-off-by: Jürgen Löhel <juergen.loehel@inlyse.com>

--- example3.py
+++ example3.py
@@ -2,39 +2,32 @@
 
 import logging
 
-from logstash_async.transport import HttpTransport
 from logstash_async.handler import AsynchronousLogstashHandler
 
 host = 'localhost'
 port = 5959
 
-logger = logging.getLogger("logstash_async.transport")
+logger = logging.getLogger('logstash_async.transport')
 logger.setLevel(logging.DEBUG)
 handler = logging.StreamHandler()
-logging_format = "%(asctime)s.%(msecs)03d"
-logging_format += " | %(levelname)-8s"
-logging_format += " | %(name)s.%(module)s.%(funcName)s"
-logging_format += " | %(message)s"
-date_format = "%Y-%m-%d | %H:%M:%S"
+logging_format = '%(asctime)s.%(msecs)03d'
+logging_format += ' | %(levelname)-8s'
+logging_format += ' | %(name)s.%(module)s.%(funcName)s'
+logging_format += ' | %(message)s'
+date_format = '%Y-%m-%d | %H:%M:%S'
 formatter = logging.Formatter(logging_format, date_format)
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
-test_logger = logging.getLogger("test")
+test_logger = logging.getLogger('test')
 test_logger.setLevel(logging.DEBUG)
-transport = HttpTransport(
-    host,
-    port,
-    ssl_enable=False,
-    timeout=5.0,
-    username="logstash",
-    password="testing",
-)
 handler = AsynchronousLogstashHandler(
     host,
     port,
     ssl_enable=False,
-    transport=transport,
+    username='logstash',
+    password='testing',
+    transport='logstash_async.transport.HttpTransport',
     database_path='logstash_test.db'
 )
 test_logger.addHandler(handler)
@@ -42,9 +35,9 @@ test_logger.addHandler(handler)
 # These tests crash logstash if the heap space is not enough
 for _ in range(11):
     test_logger.critical('A' * 10 * 1024 * 1024)
-test_logger.fatal("A" * 100 * 1024 * 1024)
+test_logger.fatal('A' * 100 * 1024 * 1024)
 
 for i in range(50):
-    test_logger.info("test %s", i)
+    test_logger.info('test %s', i)
 
-test_logger.info("end")
+test_logger.info('end')
