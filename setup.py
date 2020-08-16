d543e49f61e38f7ce6bb9dfde873d0e05f7da6c9
Enrico Tröger <enrico.troeger@uvena.de>
2020-08-16 18:57:11 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from shutil import rmtree
 import sys
 
 NAME = 'python-logstash-async'
-VERSION = '1.6.7'
+VERSION = '2.0.0'
 
 here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst'), 'rb') as f:
